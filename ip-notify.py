2c8bea2c08185ea71f528d08b6968ada4f9e0567
Dan Hill <dan@linux-dev.(none)>
2013-10-26 20:48:19 -0500

    first commit

---
+++ ip-notify.py
@@ -0,0 +1,30 @@
+import json
+from urllib2 import urlopen
+import os
+from email.mime.text import MIMEText
+from subprocess import Popen, PIPE
+
+def sendUpdate(ip):
+    performDebugging = False
+    msg = MIMEText('The new ip is: '+ ip)
+    msg['To'] = 'thirtyseventhirty@gmail.com'
+    msg['From'] = 'dan@linux-dev'
+    msg['Subject'] = 'Ip changed'
+
+    p = Popen(['/usr/sbin/sendmail', '-t'], stdin=PIPE)
+    p.communicate(msg.as_string())
+
+ip = json.load(urlopen('http://httpbin.org/ip'))['origin']    
+log = os.popen("tail -10 ip-log").readlines()
+
+print ip,
+print log,
+
+if ip in log:
+    print "they are same"
+    sendUpdate(ip)
+else:
+    print "they are not the same"
+    with open('ip-log', 'w') as f:
+        f.write(ip)
+        sendUpdate(ip)
