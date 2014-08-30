a686d83026b02586018183cb2db4cbd2c74b7e48
Dan Hill <dan@dt-deb>
2014-08-30 17:05:58 -0500

    cleaned up for reuse by other people

---
+++ README.md
@@ -0,0 +1,5 @@
+I wrote this script a few years to avoid paying for no-ip service to keep track of my home network's external IP address. It has been clean up a little and anyone is free to use it if it solves some problem they need solving.
+
+Make sure you have ssmtp correctly configured for sending out emails. I use a gmail account to forward emails from my computers.
+
+Change the address's under Configuration in the script and add it to cron job on your system and you should be good to go.
--- ip-notify.py
+++ ip-notify.py
@@ -4,21 +4,45 @@ import os
 from email.mime.text import MIMEText
 from subprocess import Popen, PIPE
 
-def sendUpdate(ip):
-    performDebugging = False
+# Configuration
+
+DEST_ADDR = "you@email.com"
+FROM_ADDR = "thismachine@email.com"
+
+def send_update(ip):
+    performDebugging = True
     msg = MIMEText('The new ip is: '+ ip)
-    msg['To'] = 'thirtyseventhirty@gmail.com'
-    msg['From'] = 'dan@linux-dev'
+    msg['To'] = DEST_ADDR
+    msg['From'] = FROM_ADDR
     msg['Subject'] = 'Ip changed'
 
     p = Popen(['/usr/sbin/sendmail', '-t'], stdin=PIPE)
     p.communicate(msg.as_string())
 
-if __name__ == "__main__":
-    ip = json.load(urlopen('http://httpbin.org/ip'))['origin']    
-    log = os.popen("tail -10 ip-log").readlines()
+def ip_log_check(ip):
+    # load the log file
+    try:
+        f = open("ip.log", "a+b")
+        log = f.readlines()
+    except IOError:
+        print "There was an error opening the log file"
+
+    # Get the last ip logged in the log file.
+    if len(log) > 0:
+        last_ip = log[-1:][0].strip("\n")
+    else:
+        last_ip = ""
+
+    if ip == last_ip:
+        return True
     
-    if not(ip in log):
-        with open('ip-log', 'w') as f:
-            f.write(ip)
-            sendUpdate(ip)
+    f.write(ip + "\n")
+    return False
+    
+
+if __name__ == "__main__":
+    ip = json.load(urlopen('http://httpbin.org/ip'))['origin']
+
+    if not(ip_log_check(ip)):
+        send_update(ip)
+
